6705eea2034364c956fb03442a7d4858f270140c
sonntu08 <son.hguyen@yara.com>
2024-07-01 13:43:21 +0700

    remove redundant package

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 import 'dotenv/config';
 
-import { readFileSync } from 'fs';
-
 import express, { Express } from 'express';
-import swaggerUi from 'swagger-ui-express';
 import bodyParser from 'body-parser';
-import passport from 'passport';
 import cors from 'cors';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import schedule from 'node-schedule';
-import swaggerJsdoc from 'swagger-jsdoc';
 import configs from './configs';
 import router from './routes';
 import { errorHandler } from './middlewares/errorHandler';
-
-const packageJson = require('../package.json');
 
 (async () => {
   const app: Express = express();
